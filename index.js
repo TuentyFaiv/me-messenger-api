326d8eeb912ad922fd3d53914828b9cb4667711d
Tonalli Lopez <aguilablanca_7@live.com>
2019-11-17 18:00:18 -0600

    terminando el archivo index.js

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const express = require('express');
+const helmet = require('helmet');
 const app = express();
 
 const { config } = require('./config/index');
-const chatsApi = require('./routes/chats');
 
+const authApi = require('./routes/auth');
+const chatsApi = require('./routes/chats');
+const contactsApi = require('./routes/contacts');
+const messagesApi = require('./routes/messages');
+
+const {
+  logErrors,
+  errorHandler,
+  wrapErrors
+} = require('./utils/middleware/errorHandlers');
+const notFoundHandler = require('./utils/middleware/notFoundHandler');
+
+//body parser
+app.use(express.json());
+app.use(helmet());
+
+//routes
+authApi(app);
+contactsApi(app);
 chatsApi(app);
+messagesApi(app);
+
+//Catch 404
+app.use(notFoundHandler);
+
+//Error middleware
+app.use(logErrors);
+app.use(wrapErrors);
+app.use(errorHandler);
 
 app.listen(config.port, function () {
   console.log(`Listening http://localhost:${config.port}`);
